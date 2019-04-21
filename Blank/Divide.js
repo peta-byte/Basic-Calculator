717708f86f9f61a9f1842e367f124430752ba2e6
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-04-20 21:25:26 -0500

    Update Divide.js

--- Blank/Divide.js
+++ Blank/Divide.js
@@ -5,18 +5,5 @@
 * and displays the result of the operation as an alert.
 */
 let divide = () => {
-  // Get and store the first and second number in variables
-  let firstNum = document.getElementById('dividend').value;
-  let secondNum = document.getElementById('divisor').value;
   
-  // Convert the numbers from their string format into integer formats
-  // then add them.
-  let result = firstNum / secondNum;
-  console.log(result);
-  // Display the sum of the numbers as an alert in the browser window
-  alert(`The result of the division of ${firstNum} by ${secondNum} is ${result}.`);
-  
-  // Clear the input fields after the calculation is completed
-  document.getElementById('dividend').value = '';
-  document.getElementById('divisor').value = '';
 };
