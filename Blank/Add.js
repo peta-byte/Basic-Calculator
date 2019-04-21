12513d3e38d440e8ac70169e726dd4b23bcc309e
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-04-20 21:25:06 -0500

    Update Add.js

--- Blank/Add.js
+++ Blank/Add.js
@@ -5,19 +5,6 @@
 * and displays the result of the operation as an alert.
 */
 let add = () => {
-  // Get and store the first and second number in variables
-  let firstNum = document.getElementById('summedUp').value;
-  let secondNum = document.getElementById('addedNum').value;
   
-  // Convert the numbers from their string format into integer formats
-  // then add them.
-  let sum = parseInt(firstNum, 10) + parseInt(secondNum, 10);
-  console.log(sum);
-  // Display the sum of the numbers as an alert in the browser window
-  alert(`The sum of ${firstNum} and ${secondNum} is ${sum}.`);
-  
-  // Clear the input fields after the calculation is completed
-  document.getElementById('summedUp').value = '';
-  document.getElementById('addedNum').value = '';
 };
 
