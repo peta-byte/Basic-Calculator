5513c976cd6672a3f33c335895260bc833e03ce9
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-04-20 21:25:45 -0500

    Update Multiply.js

--- Blank/Multiply.js
+++ Blank/Multiply.js
@@ -5,18 +5,5 @@
 * and displays the result of the operation as an alert.
 */
 let multiply = () => {
-  // Get and store the first and second number in variables
-  let firstNum = document.getElementById('multiplied').value;
-  let secondNum = document.getElementById('multiplier').value;
   
-  // Convert the numbers from their string format into integer formats
-  // then add them.
-  let result = firstNum * secondNum;
-  console.log(result);
-  // Display the sum of the numbers as an alert in the browser window
-  alert(`The result of the multiplication of ${firstNum} and ${secondNum} is ${result}.`);
-  
-  // Clear the input fields after the calculation is completed
-  document.getElementById('multiplied').value = '';
-  document.getElementById('multiplier').value = '';
 };
